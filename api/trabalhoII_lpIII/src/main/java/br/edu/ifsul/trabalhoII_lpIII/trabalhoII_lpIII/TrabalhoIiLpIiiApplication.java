22b1276b4e5c1435a8630e3e5111430b8c3bc39b
Nathan <doile.souza@gmail.com>
2023-10-04 10:26:44 -0300

    Adicionando base

---
+++ api/trabalhoII_lpIII/src/main/java/br/edu/ifsul/trabalhoII_lpIII/trabalhoII_lpIII/TrabalhoIiLpIiiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.trabalhoII_lpIII.trabalhoII_lpIII;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoIiLpIiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoIiLpIiiApplication.class, args);
+	}
+
+}
